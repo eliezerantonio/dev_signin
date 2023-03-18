d4c100e535c1556b39ae3dc819a8ea799797a42a
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-03-18 13:42:24 +0100

    configure class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); //ValidationPipe detecta as validações definidas no dto 
   await app.listen(3000);
 }
 bootstrap();
