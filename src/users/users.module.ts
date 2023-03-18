89d68c76bd255ad160afe77594db46955dcbf875
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-03-18 13:42:00 +0100

    configure useer modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserSchema } from './schemas/users.schema';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UserSchema,
+      },
+    ]),
+    AuthModule,
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
