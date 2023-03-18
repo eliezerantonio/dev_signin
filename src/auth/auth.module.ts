27c9d736051e03061094603e5c94806f7307160d
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-03-18 13:40:51 +0100

    configure auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { UserSchema } from 'src/users/schemas/users.schema';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UserSchema,
+      },
+    ]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRATION,
+      },
+    }),
+  ],
+  providers: [AuthService],
 })
 export class AuthModule {}
